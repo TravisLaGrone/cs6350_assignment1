a1a7b0f1ef3307b407220343baeaee72e762804b
TravisLaGrone <TravisLaGrone@github.com>
2017-10-08 11:33:24 -0500

    : refactor intermediateValue type
    Change from NullWritable to VIntWritable.  I had misunderstood that the
    promoted value-to-key field should not be removed from the value in
    secondary sorting.

--- src/main/java/question1/Q1Reducer.java
+++ src/main/java/question1/Q1Reducer.java
@@ -4,33 +4,33 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.VIntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class Q1Reducer extends Reducer<Q1IntermediateKey, NullWritable, Text, Text>
+public class Q1Reducer extends Reducer<Q1IntermediateKey, VIntWritable, Text, Text>
 {
 	private Integer user1;
 	private Integer user2;
 	private List<Integer> mutualFriends; 
 	
 	@Override
-	protected void setup(Reducer<Q1IntermediateKey, NullWritable, Text, Text>.Context context)
+	protected void setup(Reducer<Q1IntermediateKey, VIntWritable, Text, Text>.Context context)
 			throws IOException, InterruptedException
 	{
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	protected void reduce(Q1IntermediateKey arg0, Iterable<NullWritable> arg1,
-			Reducer<Q1IntermediateKey, NullWritable, Text, Text>.Context arg2) throws IOException, InterruptedException
+	protected void reduce(Q1IntermediateKey arg0, Iterable<VIntWritable> arg1,
+			Reducer<Q1IntermediateKey, VIntWritable, Text, Text>.Context arg2) throws IOException, InterruptedException
 	{
 		// TODO Auto-generated method stub
 		super.reduce(arg0, arg1, arg2);
 	}
 	
 	@Override
-	protected void cleanup(Reducer<Q1IntermediateKey, NullWritable, Text, Text>.Context context)
+	protected void cleanup(Reducer<Q1IntermediateKey, VIntWritable, Text, Text>.Context context)
 			throws IOException, InterruptedException
 	{
 		// TODO Auto-generated method stub
